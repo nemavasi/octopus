3eaf5e36ae993f47979084dc19094780ffc8b146
nemavasi <thatbook2@gmail.com>
2016-01-18 13:49:40 +0300

    003

--- src/test/java/com/ds/my/www/common/IPosition.java
+++ src/test/java/com/ds/my/www/common/IPosition.java
@@ -1,8 +1,11 @@
 package com.ds.my.www.common;
 
+import java.util.List;
+
 /**
  * @author dmitry shalygin on 1/18/2016.
  */
 public interface IPosition {
     PositionStatus getStatus();
+    List<IPosition> getSubPositions();
 }
