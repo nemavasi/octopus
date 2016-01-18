c8ca4ae526512be16273bf7b4a0838c92561c770
nemavasi <thatbook2@gmail.com>
2016-01-18 12:06:51 +0300

    002

---
+++ src/test/java/com/ds/my/www/common/IPosition.java
@@ -0,0 +1,8 @@
+package com.ds.my.www.common;
+
+/**
+ * @author dmitry shalygin on 1/18/2016.
+ */
+public interface IPosition {
+    PositionStatus getStatus();
+}
---
+++ src/test/java/com/ds/my/www/common/PositionStatus.java
@@ -0,0 +1,11 @@
+package com.ds.my.www.common;
+
+/**
+ * @author dmitry shalygin on 1/18/2016.
+ */
+public enum PositionStatus {
+    PLANNED,
+    TAKING,
+    TAKEN,
+    PROTECTING
+}
