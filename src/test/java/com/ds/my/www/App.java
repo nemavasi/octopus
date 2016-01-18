1bfa37bdab7647869b4b0e921c1d08a7c0d4bded
Dmitry Shalygin <thatbook2@gmail.com>
2016-01-18 08:53:51 +0300

    first

---
+++ src/test/java/com/ds/my/www/App.java
@@ -0,0 +1,12 @@
+package com.ds.my.www;
+
+/**
+ * Created by nemavasi on 18.01.16.
+ */
+public class App {
+
+    public static void main(String args[]){
+        System.out.println("sdfsadf");
+    }
+
+}
